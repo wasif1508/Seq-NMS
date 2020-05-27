51fad36f4172574b5c516d2e117faafa188dee71
Wasif Jawad Hussain <44579117+wasif1508@users.noreply.github.com>
2020-05-28 00:59:17 +0530

    Update seqnms.py

--- seqnms.py
+++ seqnms.py
@@ -28,10 +28,10 @@ IOU_THRESH = 0.6
 '''
 def createInputs(video):
     create_begin=time.time()
-    frames=sorted(video.keys()) #获得按序排列的帧的名称
-    dets=[[] for i in CLASSES[1:]] #保存最终结果
-    for cls_ind,cls in enumerate(CLASSES[1:]): #类
-        for frame_ind,frame in enumerate(frames): #帧
+    frames=sorted(video.keys()) #
+    dets=[[] for i in CLASSES[1:]] # 
+    for cls_ind,cls in enumerate(CLASSES[1:]): #
+        for frame_ind,frame in enumerate(frames): #
             cls_boxes = video[frame]['boxes'][:, 4*(cls_ind+1):4*(cls_ind + 2)]
             cls_scores = video[frame]['scores'][:, cls_ind+1]
             cls_dets = np.hstack((cls_boxes,cls_scores[:, np.newaxis])).astype(np.float64)
@@ -42,30 +42,28 @@ def createInputs(video):
 
 def createLinks(dets_all):
     links_all=[]
-    #建立每相邻两帧之间的link关系
     frame_num=len(dets_all[0])
     cls_num=len(CLASSES)-1
-    #links_all=[] #保存每一类的全部link，第一维为类数，第二维为帧数-1，为该类下的links即每一帧与后一帧之间的link，第三维每帧的box数，为该帧与后一帧之间的link
-    for cls_ind in range(cls_num): #第一层循环，类数
-        links_cls=[] #保存一类下全部帧的links
+    for cls_ind in range(cls_num): 
+        links_cls=[] #links
         link_begin=time.time()
-        for frame_ind in range(frame_num-1): #第二层循环，帧数-1，不循环最后一帧
+        for frame_ind in range(frame_num-1): 
             dets1=dets_all[cls_ind][frame_ind]
             dets2=dets_all[cls_ind][frame_ind+1]
             box1_num=len(dets1)
             box2_num=len(dets2)
-            #先计算每个box的area
+            #个box area
             if frame_ind==0:
                 areas1=np.empty(box1_num)
                 for box1_ind,box1 in enumerate(dets1):
                     areas1[box1_ind]=(box1[2]-box1[0]+1)*(box1[3]-box1[1]+1)
-            else: #当前帧的area1就是前一帧的area2，避免重复计算
+            else: #area1 area2
                 areas1=areas2
             areas2=np.empty(box2_num)
             for box2_ind,box2 in enumerate(dets2):
                 areas2[box2_ind]=(box2[2]-box2[0]+1)*(box2[3]-box2[1]+1)
-            #计算相邻两帧同一类的link
-            links_frame=[] #保存相邻两帧的links
+            
+            links_frame=[] 
             for box1_ind,box1 in enumerate(dets1):
                 area1=areas1[box1_ind]
                 x1=np.maximum(box1[0],dets2[:,0])
@@ -76,7 +74,7 @@ def createLinks(dets_all):
                 h =np.maximum(0.0, y2 - y1 + 1)
                 inter = w * h
                 ovrs = inter / (area1 + areas2 - inter)
-                links_box=[ovr_ind for ovr_ind,ovr in enumerate(ovrs) if ovr >= IOU_THRESH] #保存第一帧的一个box对第二帧全部box的link
+                links_box=[ovr_ind for ovr_ind,ovr in enumerate(ovrs) if ovr >= IOU_THRESH] #box box link
                 links_frame.append(links_box)
             links_cls.append(links_frame)
         link_end=time.time()
@@ -104,14 +102,14 @@ def NMS(dets_all):
             dets_all[cls_ind][frame_ind]=dets[keep, :]
 
 def findMaxPath(links,dets):
-    maxpaths=[] #保存从每个结点到最后的最大路径与分数
-    roots=[] #保存所有的可作为独立路径进行最大路径比较的路径
+    maxpaths=[]
+    roots=[] 
     maxpaths.append([ (box[4],[ind]) for ind,box in enumerate(dets[-1])])
-    for link_ind,link in enumerate(links[::-1]): #每一帧与后一帧的link，为一个list
+    for link_ind,link in enumerate(links[::-1]): #link，list
         curmaxpaths=[]
         linkflags=np.zeros(len(maxpaths[0]),int)
         det_ind=len(links)-link_ind-1
-        for ind,linkboxes in enumerate(link): #每一帧中每个box的link，为一个list
+        for ind,linkboxes in enumerate(link): #box,ink，list
             if linkboxes == []:
                 curmaxpaths.append((dets[det_ind][ind][4],[ind]))
                 continue
@@ -155,18 +153,18 @@ def deleteLink(dets,links, rootindex, maxpath,thesh):
         h =np.maximum(0.0, y2 - y1 + 1)
         inter = w * h
         ovrs = inter / (area1 + areas - inter)
-        deletes=[ovr_ind for ovr_ind,ovr in enumerate(ovrs) if ovr >= IOU_THRESH] #保存待删除的box的index
-        if rootindex+i<len(links): #除了最后一帧，置box_ind的box的link为空
+        deletes=[ovr_ind for ovr_ind,ovr in enumerate(ovrs) if ovr >= IOU_THRESH] #box index
+        if rootindex+i<len(links): #box_ind box link
             for delete_ind in deletes:
                 links[rootindex+i][delete_ind]=[]
         if i > 0 or rootindex>0:
-            for priorbox in links[rootindex+i-1]: #将前一帧指向box_ind的link删除
+            for priorbox in links[rootindex+i-1]: #box_ind link
                 for delete_ind in deletes:
                     if delete_ind in priorbox:
                         priorbox.remove(delete_ind)
 
 def saveforAP(dets_all,frame_names):
-    #保存结果为用于计算ap的格式
+   
     for cls_ind, cls_name in enumerate(CLASSES):
         if cls_name == '__background__':
             continue
